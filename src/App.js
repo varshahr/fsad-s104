aecdb22c902cef38b3704bb88e43252de9695e4a
sri varsha <srivarshadayana@gmail.com>
2025-10-16 19:26:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -3,22 +3,13 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 align ="center"> This is Git deployment test
+      </h1>
+    
+    
     </div>
+   
   );
 }
 
